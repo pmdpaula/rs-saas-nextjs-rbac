a197ffc78af97f66dc8045ab665e7199a20a8001
Pedro Machado de Paula <pmdpaula@gmail.com>
2025-09-29 17:10:23 -0300

    feat(apps/api): Refactor code structure for improved readability and maintainability

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilityFor } from "@saas/auth";
-
-const ability = defineAbilityFor({role: "ADMIN"});
-
-const useCanInviteSomeoneElse = ability.can("invite", "User");
-
-console.log("🚀 ~ file: index.ts:3 ~ useCanInviteSomeoneElse:", useCanInviteSomeoneElse);
