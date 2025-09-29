e2d0acee58adec2ee468f520e918665853fdb5fa
Pedro Machado de Paula <pmdpaula@gmail.com>
2025-09-29 17:24:47 -0300

    chore: update dependencies and add new Fastify plugins
    - Added '@fastify/swagger' (v9.5.2) and '@fastify/swagger-ui' (v5.2.3) to the project.
    - Included '@fastify/accept-negotiator' (v2.0.1), '@fastify/send' (v4.1.0), and '@fastify/static' (v8.2.0) in the lock file.
    - Updated various dependencies including 'ansi-regex', 'string-width', and 'wrap-ansi' to their latest versions.
    - Ensured compatibility with Node.js versions in the engines field for several packages.

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,4 +1,6 @@
 import fastifyCors from "@fastify/cors";
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
 import { fastify } from 'fastify';
 import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 
@@ -8,6 +10,22 @@ const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-Stack SaaS app with multi-tenancy and RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+});
 
 app.register(fastifyCors, {
   origin: '*',
