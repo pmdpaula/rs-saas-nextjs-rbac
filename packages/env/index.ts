938288ffd1953498a736e829f8de951941567db4
Pedro Machado de Paula <pmdpaula@gmail.com>
2025-09-30 17:38:46 -0300

    feat(env): initialize environment configuration with zod validation

---
+++ packages/env/index.ts
@@ -0,0 +1,40 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    SERVER_PORT: z.coerce.number().default(3333),
+
+    DATABASE_URL: z.url(),
+    DATABASE_API_KEY: z.string().min(64),
+
+    JWT_SECRET: z.string().min(32),
+    JWT_EXPIRES_IN: z.string().default("15m"),
+
+    REFRESH_TOKEN_SECRET: z.string().min(32),
+    REFRESH_TOKEN_EXPIRES_IN: z.string().default("7d"),
+
+    GITHUB_OAUTH_CLIENT_ID: z.string(),
+    GITHUB_OAUTH_CLIENT_SECRET: z.string(),
+    GITHUB_OAUTH_REDIRECT_URI: z.url(),
+  },
+  client: {},
+  shared: {},
+  runtimeEnv: {
+    SERVER_PORT: process.env.SERVER_PORT,
+
+    DATABASE_API_KEY: process.env.DATABASE_API_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
+
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN,
+
+    GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
+    GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
+    GITHUB_OAUTH_REDIRECT_URI: process.env.GITHUB_OAUTH_REDIRECT_URI,
+  },
+  emptyStringAsUndefined: true,
+});
