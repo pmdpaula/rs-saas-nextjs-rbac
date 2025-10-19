8517d8b363b52e6074c77b5ef7fed588ca32a372
Pedro Machado de Paula <pmdpaula@gmail.com>
2025-10-19 13:14:12 -0300

    feat(layout): add AppLayout component for authentication redirection; create Home component with Header

---
+++ apps/web/src/app/(app)/page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header";
+
+const Home = async () => {
+  return (
+    <div className="py-4">
+      <Header />
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
--- apps/web/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/auth/auth";
-
-const Home = async () => {
-  const user = await auth();
-  console.log("🚀 ~ Home ~ user:", user);
-  // return <pre>{JSON.stringify(user, null, 2)}</pre>;
-};
-
-export default Home;
