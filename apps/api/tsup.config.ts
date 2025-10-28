258bea0a66e9fd9198ab08a9b1ebce69f18cb95e
Pedro Machado de Paula <pmdpaula@gmail.com>
2025-10-28 14:35:32 -0300

    Refactor code structure for improved readability and maintainability

---
+++ apps/api/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  outDir: "dist",
+  noExternal: ["@saas-auth", "@saas/env"],
+});
